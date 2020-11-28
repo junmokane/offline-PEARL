b6119ea2d56b3dbf039f4e4f2c84b5a21f5c11fe
junmokane <junmokane@kaist.ac.kr>
2020-11-28 16:36:27 +0900

    halfcheetah-dir

--- rlkit/envs/half_cheetah_dir.py
+++ rlkit/envs/half_cheetah_dir.py
@@ -60,3 +60,49 @@ class HalfCheetahDirEnv(HalfCheetahEnv):
         self._goal_dir = self._task['direction']
         self._goal = self._goal_dir
         self.reset()
+
+
+@register_env('cheetah-fwd')
+class HalfCheetahFwdEnv(HalfCheetahEnv):
+    def __init__(self):
+        self._goal_dir = 1
+        super(HalfCheetahFwdEnv, self).__init__()
+
+    def step(self, action):
+        xposbefore = self.sim.data.qpos[0]
+        self.do_simulation(action, self.frame_skip)
+        xposafter = self.sim.data.qpos[0]
+
+        forward_vel = (xposafter - xposbefore) / self.dt
+        forward_reward = self._goal_dir * forward_vel
+        ctrl_cost = 0.5 * 1e-1 * np.sum(np.square(action))
+
+        observation = self._get_obs()
+        reward = forward_reward - ctrl_cost
+        done = False
+        infos = dict(reward_forward=forward_reward,
+            reward_ctrl=-ctrl_cost, task=self._task)
+        return (observation, reward, done, infos)
+
+
+@register_env('cheetah-bwd')
+class HalfCheetahBwdEnv(HalfCheetahEnv):
+    def __init__(self):
+        self._goal_dir = -1
+        super(HalfCheetahBwdEnv, self).__init__()
+
+    def step(self, action):
+        xposbefore = self.sim.data.qpos[0]
+        self.do_simulation(action, self.frame_skip)
+        xposafter = self.sim.data.qpos[0]
+
+        forward_vel = (xposafter - xposbefore) / self.dt
+        forward_reward = self._goal_dir * forward_vel
+        ctrl_cost = 0.5 * 1e-1 * np.sum(np.square(action))
+
+        observation = self._get_obs()
+        reward = forward_reward - ctrl_cost
+        done = False
+        infos = dict(reward_forward=forward_reward,
+            reward_ctrl=-ctrl_cost, task=self._task)
+        return (observation, reward, done, infos)
